561a2ca1b74865e84c4c6849637ac79194dde942
Moldir Azhimukhanbet <azhimukhanbetmoldir@gmail.com>
2024-12-12 15:48:03 +0500

    tb works

--- test/tb.v
+++ test/tb.v
@@ -10,16 +10,20 @@ module tb ();
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
-    #1;
+    //#1;
   end
 
   // Wire up the inputs and outputs:
   reg clk;
-  reg reset;
+  reg rst_n;
   reg ena;
   reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
+  reg [4:0] uio_in_7to3;
+  reg [1:0] uio_in_2to1;
+  reg uio_in_0;
+
+  wire [6:0] uo_out_7to1;
+  wire uo_out_0;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
@@ -33,13 +37,13 @@ module tb ();
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
+      .uo_out ({uo_out_7to1, uo_out_0}),   // Dedicated outputs
+      .uio_in ({uio_in_7to3, uio_in_2to1, uio_in_0}),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (reset)     // not reset
+      .rst_n  (rst_n)     // not reset
   );
 
 endmodule
